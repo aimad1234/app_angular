80f185923e7344c6774347540c97d072beeeb464
AIMAD EL HAMMDANI <66996795+aimad1234@users.noreply.github.com>
2024-11-06 15:56:33 +0000

    add thumbnail child

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,31 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div class="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+    <div
+      class="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden"
+    >
       <img
         class="w-32 h-32 rounded-full"
         [src]="event.image_url"
         alt="{{ event.name }}"
       />
       <div class="p-6">
-        <h2 class="text-2xl font-bold mb-2">{{ event.name }}</h2>
+        <h2 class="text-gray-800 font-bold mb-2">{{ event.name }}</h2>
         <p class="text-gray-600 mb-4">{{ event.description }}</p>
         <div class="flex justify-between items-center">
           <div>
             <p class="text-gray-700">
               <strong>Date:</strong> {{ event.date | date : 'fullDate' }}
             </p>
-            <p class="text-gray-700">
+            <p class="text-red-500">
               <strong>Location:</strong> {{ event.location.address }},
               {{ event.location.city }}, {{ event.location.country }}
             </p>
           </div>
-          <button
-            class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          <!-- Primary button style -->
+          <!-- <button
+            class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            (click)="handleClickme()"
           >
-            En savoir plus
+            Click me !
+          </button> -->
+          <button
+            class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            (click)="handleClickme()"
+          >
+            Click me !
           </button>
         </div>
       </div>
@@ -34,4 +44,13 @@ import { Component, Input } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+  // Création de l'EventEmitter et déclaration de l'événement avec @Output
+  @Output() eventClick = new EventEmitter();
+  handleClickme() {
+    this.eventClick.emit({ name: 'aimad', code: 'S135316719' });
+  }
+  props: string = 'Child Component';
+  // handleMethod() {
+  //   console.log('Hello');
+  // }
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,18 +3,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-list',
   template: `
-    <div>
-      <h2>Composant Parent</h2>
-      <event-thumbnail [event]="event1"></event-thumbnail>
+    <!-- <header class="bg-white-500 text-white p-4">
+      <h2 class="text-3xl font-semibold text-gray-500 text-center">
+        Parent Composant
+      </h2>
       <hr />
-    </div>
+    </header> -->
+    <header class="bg-gray-500">
+      <nav class="max-w-screen-sm mx-auto p-4">
+        <div class="flex items-center justify-between">
+          <!-- Logo -->
+          <div class="text-white text-xl font-semibold">
+            <a href="#">Logo</a>
+          </div>
+          <!-- Navigation Links -->
+          <div class="hidden sm:flex space-x-4">
+            <a
+              href="#"
+              class="text-white px-3 py-2 text-sm font-medium hover:bg-blue-700 rounded"
+              >Home</a
+            >
+            <a
+              href="#"
+              class="text-white px-3 py-2 text-sm font-medium hover:bg-blue-700 rounded"
+              >About</a
+            >
+            <a
+              href="#"
+              class="text-white px-3 py-2 text-sm font-medium hover:bg-blue-700 rounded"
+              >Services</a
+            >
+            <a
+              href="#"
+              class="text-white px-3 py-2 text-sm font-medium hover:bg-blue-700 rounded"
+              >Contact</a
+            >
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- <event-thumbnail
+        [event]="event1"
+        (eventClick)="handleEventClicked($event)"
+      ></event-thumbnail> -->
+    <!-- <h4 class="text-1xl font-semibold text-gray-600 text-center">
+      {{ thumbnail.props
+      }}
+    </h4> -->
+    <event-thumbnail
+      [event]="event1"
+      #thumbnail
+      (eventClick)="handleEventClicked($event)"
+    ></event-thumbnail>
   `,
 })
 export class EventsListComponent {
   title = 'Event List Component';
   event1 = {
     id: 1,
-    name: 'Angular Workshop 1',
+    name: 'Angular Workshop',
     date: new Date(2024, 11, 15), // December 15, 2024
     location: {
       address: '123 Angular Lane',
@@ -24,4 +71,7 @@ export class EventsListComponent {
     image_url: './assets/angular-logo.png',
     description: 'A workshop on Angular best practices and new features.',
   };
+  handleEventClicked(data: any) {
+    console.log('receveid', data);
+  }
 }
